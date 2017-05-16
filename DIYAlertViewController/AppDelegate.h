21e138fafde5d177df941ad47c011e35a8525037
Lemonade <842476697@qq.com>
2017-05-16 16:50:32 +0800

    Initial Commit

---
+++ DIYAlertViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DIYAlertViewController
+//
+//  Created by Lemonade on 2017/5/16.
+//  Copyright © 2017年 Lemonade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
